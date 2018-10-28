c2f008c46fbd486a89d9089273772effbbe15d6c
Brian Bancroft <hello@brianbancroft.ca>
2018-10-27 21:06:01 -0400

    ✨  Use contentful for home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,44 @@
 import React from 'react'
-
+import { Link } from 'gatsby'
 import Layout from '../components/layout'
 
-const IndexPage = () => (
-  <Layout>
-    <section>
-      <h1>Welcome To Anne-Lynn Design</h1>
-    </section>
-    <section>
-      <p>This is where the content comes from</p>
-    </section>
-    <section>This is the about section...</section>
-  </Layout>
-)
+const IndexPage = props => {
+  console.log('test test', props)
+
+  const porfolioItems = props.data.allContentfulPortfolioItem.edges.map(i => (
+    <li>
+      <Link to={`/portfolio/${i.node.slug}}`}>{i.node.title}</Link>
+    </li>
+  ))
+
+  return (
+    <Layout>
+      <section>
+        <h1>Welcome To Anne-Lynn Design</h1>
+      </section>
+      <section>
+        <p>This is where the content comes from</p>
+        <ol>{porfolioItems}</ol>
+      </section>
+      <section>This is the about section...</section>
+    </Layout>
+  )
+}
 
 export default IndexPage
+
+export const pageQuery = graphql`
+  query HomeQuery {
+    allContentfulPortfolioItem {
+      edges {
+        node {
+          id
+          title
+          slug
+
+          createdAt(formatString: "MMM Do, YYYY")
+        }
+      }
+    }
+  }
+`
