2f6c0b9571a61238ab5642a863510c9893442468
Brian Bancroft <hello@brianbancroft.ca>
2018-11-03 12:37:51 -0400

    🐛  Copy failsafe

--- src/components/portfolio/color-and-typography.js
+++ src/components/portfolio/color-and-typography.js
@@ -13,13 +13,13 @@ const imagesLayout = images =>
   images.map(i => <img src={i.file.url} key={i.id} />)
 
 const ColorAndTypography = ({ colorAndTypography } = {}) => {
-  console.log('Colour and typography pane', colorAndTypography)
+  // console.log('Colour and typography pane', colorAndTypography)
 
   const sections = colorAndTypography.map(i => (
     <ColorTypographySection key={i.id}>
       {imagesLayout(i.images)}
       <h1>Colors and Typefaces</h1>
-      <div>{i.copy.copy}</div>
+      <div>{() => (i.copy ? i.copy.copy || '' : '')}</div>
     </ColorTypographySection>
   ))
 
--- src/components/portfolio/supporting-concepts.js
+++ src/components/portfolio/supporting-concepts.js
@@ -1,8 +1,32 @@
 import React from 'react'
+import styled from 'react-emotion'
+
+const SupportingConceptsSection = styled('section')`
+  width: 100vw;
+  height: 100vh;
+  background: white;
+  outline-color: purple;
+  outline-style: dashed;
+`
 
 const SupportingConcepts = ({ supportingConcepts } = {}) => {
-  // console.log('Supporting concepts pane -> ', supportingConcepts)
-  return <div>Hello supporting concepts</div>
+  console.log('Supporting concepts pane -> ', supportingConcepts)
+  const sections = supportingConcepts.map(i => (
+    <SupportingConceptsSection key={i.id}>
+      <div>{i.title}</div>
+      <div>
+        Aliqua aliquip magna elit elit velit culpa eiusmod. Id non dolore nisi
+        tempor duis quis sit esse occaecat sint excepteur reprehenderit ut.
+        Cillum elit cupidatat occaecat incididunt tempor officia ut ut consequat
+        esse sit. Incididunt aliqua magna nostrud exercitation nostrud elit
+        consequat esse laboris do Lorem pariatur veniam est. Laboris excepteur
+        aliquip pariatur nisi elit ipsum consequat id officia Lorem ullamco
+        laboris consectetur.
+      </div>
+    </SupportingConceptsSection>
+  ))
+
+  return <>{sections}</>
 }
 
 export default SupportingConcepts
