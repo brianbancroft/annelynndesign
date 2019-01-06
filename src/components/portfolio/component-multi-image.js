7ae677390c61357ff2270406ffe3e6bafc279470
Brian Bancroft <hello@brianbancroft.ca>
2019-01-06 14:23:51 -0500

    progress

--- src/components/portfolio/component-multi-image.js
+++ src/components/portfolio/component-multi-image.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import styled from 'react-emotion'
 
 const MultiImageSection = styled('section')`
@@ -63,28 +63,85 @@ const ImageContainer = styled('div')`
   margin-right: 5px;
 `
 
-const imagesLayout = images =>
-  images.map(i => <ImageContainer src={i.fluid.src} key={i.id} />)
+const Modal = styled('div')`
+  position: fixed;
+  width: 100vw;
+  height: 100vh;
+  background: rgba(0, 0, 0, 0.3);
+  z-index: 123;
+  top: 0;
 
-const Copy = ({ copy } = {}) => {
-  const inner = copy ? copy.copy || '' : ''
-  return <div>{inner}</div>
+  color: white;
+
+  display: grid;
+  flex: 1; /* fg:1, fs:1, fb:0 */
+
+  grid-template-rows: 25px 50px 1fr 50px;
+  grid-template-columns: 20px 70px 1fr 70px 20px;
+  align-items: center;
+
+  h1.title {
+    text-align: center;
+    grid-row: 2 / 3;
+    grid-column: 1 / 6;
+  }
+  .images-container {
+    grid-row: 3 / 4;
+    grid-column: 3 / 4;
+    background: white;
+    min-height: 700px;
+  }
+`
+
+class ComponentMultiImage extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      showModal: false,
+    }
+  }
+
+  render() {
+    const { color, images, copy, cssClasses, title } = this.props
+
+    const handleOpen = () => this.setState({ showModal: true })
+    const handleClose = () => this.setState({ showModal: false })
+
+    const imagesLayout = images =>
+      images.map(i => (
+        <ImageContainer src={i.fluid.src} key={i.id} onClick={handleOpen} />
+      ))
+
+    const Copy = ({ copy } = {}) => {
+      const inner = copy ? copy.copy || '' : ''
+      return <div>{inner}</div>
+    }
+
+    const ImageModal = ({ title, images } = {}) =>
+      this.state.showModal ? (
+        <Modal onClick={handleClose}>
+          <h1 className="title">This is a modal title</h1>
+          {/* <div className="previous-select">Left</div> */}
+          <div className="images-container" />
+          {/* <div className="right-select">Right</div> */}
+        </Modal>
+      ) : (
+        <></>
+      )
+
+    return (
+      <>
+        <ImageModal id="imageModal" />
+        <MultiImageSection color={color}>
+          <div className="images-container">{imagesLayout(images)}</div>
+          <div className="title">{title}</div>
+          <div className="copy">
+            <Copy copy={copy} />
+          </div>
+        </MultiImageSection>
+      </>
+    )
+  }
 }
-
-const ComponentMultiImage = ({
-  color,
-  images,
-  copy,
-  cssClasses,
-  title,
-} = {}) => (
-  <MultiImageSection color={color}>
-    <div className="images-container">{imagesLayout(images)}</div>
-    <div className="title">{title}</div>
-    <div className="copy">
-      <Copy copy={copy} />
-    </div>
-  </MultiImageSection>
-)
 
 export default ComponentMultiImage
