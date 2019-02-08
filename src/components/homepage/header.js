a2f564b49dbed73d380e54e8d42fae57d154d103
Brian Bancroft <hello@brianbancroft.ca>
2019-02-08 09:02:30 -0800

    ♻️  Use styled components over classes

--- src/components/homepage/header.js
+++ src/components/homepage/header.js
@@ -11,108 +11,101 @@ const HeaderSection = styled('section')`
   top: 0;
   position: fixed;
   font-family: 'Raleway', sans-serif;
+`
 
-  .title {
-    user-select: none;
-    padding: 0 40px;
-    padding-top: calc(100vh / 14);
-    z-index: 10;
-    transform: scale(1, 1.2);
-    text-shadow: white 0.1em 0.1em 0.1em;
-    font-size: 90px;
-    text-transform: uppercase;
-    line-height: 90px;
-    font-weight: 100;
-    color: #1e9dba;
-  }
-
-  .circle {
-    border-radius: 50%;
-    z-index: -11;
-  }
-
-  .circle-one {
-    border: 1px solid #1e9dba;
-    height: 650px;
-    width: 650px;
-    position: absolute;
-    right: 30px;
-    top: 420px;
-  }
-
-  .circle-two {
-    border: 1px solid #006a77;
-    height: 300px;
-    width: 300px;
-    position: absolute;
-    right: 40px;
-    top: 50px;
-  }
-
-  .circle-three {
-    border: 1px solid #8ec73f;
-    height: 200px;
-    width: 200px;
-    position: absolute;
-    right: 40px;
-    top: 330px;
-  }
+const TitleElement = styled('div')`
+  user-select: none;
+  padding: 0 40px;
+  padding-top: calc(100vh / 14);
+  z-index: 10;
+  transform: scale(1, 1.2);
+  text-shadow: white 0.1em 0.1em 0.1em;
+  font-size: 90px;
+  text-transform: uppercase;
+  line-height: 90px;
+  font-weight: 100;
+  color: #1e9dba;
 
   @media (max-width: 620px) {
-    .title {
-      text-transform: uppercase;
-      font-size: 14vw;
-      color: #1e9dba;
-      padding-top: 15px;
-      padding-left: 10px;
-      letter-spacing: -0.39px;
-      line-height: 70px;
-      text-shadow: 0 0 0.2em #fff;
-      z-index: 10;
-    }
+    text-transform: uppercase;
+    font-size: 14vw;
+    color: #1e9dba;
+    padding-top: 15px;
+    padding-left: 10px;
+    letter-spacing: -0.39px;
+    line-height: 70px;
+    text-shadow: 0 0 0.2em #fff;
+    z-index: 10;
+  }
+`
 
-    .circle-one {
-      border: 1px solid #1e9dba;
-      height: 70vw;
-      width: 70vw;
-      position: absolute;
-      left: -40px;
-      top: 400px;
-    }
+const Circle = styled('div')`
+  border-radius: 50%;
+  z-index: -11;
+  position: absolute;
+`
 
-    .circle-two {
-      border: 1px solid #006a77;
-      height: 40vw;
-      width: 40vw;
-      position: absolute;
-      left: 220px;
-      top: 400px;
-    }
+const CircleOne = styled(Circle)`
+  border: 1px solid #1e9dba;
+  height: 650px;
+  width: 650px;
+  right: 30px;
+  top: 420px;
 
-    .circle-three {
-      border: 1px solid #8ec73f;
-      height: 65vw;
-      width: 55vw;
-      position: absolute;
-      left: 220px;
-      top: 560px;
-    }
+  @media (max-width: 620px) {
+    border: 1px solid #1e9dba;
+    height: 70vw;
+    width: 70vw;
+    left: -40px;
+    top: 400px;
+  }
+`
+
+const CircleTwo = styled(Circle)`
+  border: 1px solid #006a77;
+  height: 300px;
+  width: 300px;
+  right: 40px;
+  top: 50px;
+
+  @media (max-width: 620px) {
+    border: 1px solid #006a77;
+    height: 40vw;
+    width: 40vw;
+    left: 220px;
+    top: 400px;
+  }
+`
+
+const CircleThree = styled(Circle)`
+  border: 1px solid #8ec73f;
+  height: 200px;
+  width: 200px;
+  right: 40px;
+  top: 330px;
+
+  @media (max-width: 620px) {
+    border: 1px solid #8ec73f;
+    height: 65vw;
+    width: 55vw;
+    left: 220px;
+    top: 560px;
   }
 `
 
 class Header extends React.Component {
-  render () {
+  render() {
     return (
       <HeaderSection id="headerSection">
-        <div className="welcome-header">
-          <div className="title">welcome</div>
-          <div className="title">to</div>
-          <div className="title">anne-lynn</div>
-          <div className="title">design</div>
+        <div>
+          <TitleElement>welcome</TitleElement>
+          <TitleElement>to</TitleElement>
+          <TitleElement>anne-lynn</TitleElement>
+          <TitleElement>design</TitleElement>
         </div>
-        <div className="circle circle-one" />
-        <div className="circle circle-two" />
-        <div className="circle circle-three" />
+        <CircleOne />
+        <CircleTwo />
+        <CircleThree />
         <ScrollIcon />
       </HeaderSection>
     )
