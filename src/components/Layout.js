fba26c640a8ab7e3636e7fc3d5aa140a5e8d703c
Brian Bancroft <hello@brianbancroft.ca>
2019-04-28 14:37:52 -0400

    🔥  Remove unused parts from normalize
    Shrinks bundle size

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,20 +1,25 @@
 import React from 'react'
 import { ThemeProvider } from 'emotion-theming'
 import Helmet from 'react-helmet'
+import styled from '@emotion/styled'
 import './layout.css'
 import { theme } from '../helpers'
 
 import { meta, helmetLink } from './layout-meta'
 import { Footer } from '.'
 
+const Site = styled('main')``
+
 const Layout = ({ children, location = '/', portfolioHeaderColor } = {}) => (
-  <ThemeProvider theme={theme}>
-    <Helmet link={helmetLink} meta={meta}>
-      <html lang="en" />
-    </Helmet>
-    {children}
-    <Footer portfolioHeaderColor={portfolioHeaderColor} />
-  </ThemeProvider>
+  <Site>
+    <ThemeProvider theme={theme}>
+      <Helmet link={helmetLink} meta={meta}>
+        <html lang="en" />
+      </Helmet>
+      {children}
+      <Footer portfolioHeaderColor={portfolioHeaderColor} />
+    </ThemeProvider>
+  </Site>
 )
 
 export default Layout
