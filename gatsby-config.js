2d45300a7c5334d8cf4573a71a5868d30be1ed52
Brian Bancroft <hello@brianbancroft.ca>
2019-05-26 12:56:35 -0700

    🔎 Use only tracking id in google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -69,24 +69,6 @@ const moduleExports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: 'UA-119666699-9',
-        // Puts tracking script in the head instead of the body
-        head: false,
-        // Setting this parameter is optional
-        // anonymize: true,
-        // Setting this parameter is also optional
-        // respectDNT: true,
-        // Avoids sending pageview hits from custom paths. Array of paths
-        // exclude: [],
-        // Enables Google Optimize using your container Id
-        // optimizeId: 'YOUR_GOOGLE_OPTIMIZE_TRACKING_ID',
-        // Enables Google Optimize Experiment ID
-        // experimentId: 'YOUR_GOOGLE_EXPERIMENT_ID',
-        // Set Variation ID. 0 for original 1,2,3....
-        // variationId: 'YOUR_GOOGLE_OPTIMIZE_VARIATION_ID',
-        // Any additional create only fields (optional)
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
-        cookieDomain: 'annelynn.com',
       },
     },
     `gatsby-plugin-netlify`, // ! Ensure this is the last line of the config array
