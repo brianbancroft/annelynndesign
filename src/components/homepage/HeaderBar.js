125a5e98b6b25c8e99e544233ddf272e285888fa
Brian Bancroft <hello@brianbancroft.ca>
2020-01-04 15:43:00 -0800

    💄  Tweak headerbar attributes

--- src/components/homepage/HeaderBar.js
+++ src/components/homepage/HeaderBar.js
@@ -4,19 +4,23 @@ import logo from '../../images/homepage-logo.png'
 
 const Bar = styled('div')`
   width: 100vw;
-  height: 120px;
+  height: 100px;
   display: flex;
   justify-content: center;
   align-items: center;
   background: #e8fbf9;
   margin-bottom: 40px;
 
+  img {
+    width: 55px;
+  }
+
   @media (max-width: ${props => props.theme.mobileBreakpoint}) {
     margin-bottom: 25px;
     height: 90px;
 
     img {
-      width: 50px;
+      width: 40px;
     }
   }
 `
