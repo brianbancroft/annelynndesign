30fb033fe72c93683e869331ff8f2f16f90dfc5d
Brian Bancroft <hello@brianbancroft.ca>
2020-10-09 20:59:42 -0700

    ✨  Implementent rectangle cases for single image display

--- src/components/portfolio/CaseStudySectionSingleImage.js
+++ src/components/portfolio/CaseStudySectionSingleImage.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+import Img from 'gatsby-image'
+import { convertCSSToRGB, getValueFromRGB, lightOrDark } from '../../helpers'
+import { compose } from 'ramda'
 
 const MockSection = styled.section`
   width: 100vw;
@@ -9,13 +12,83 @@ const MockSection = styled.section`
   outline-color: salmon;
 `
 
-function CaseStudySectionSingleImage(props) {
-  const { element, color } = props
-  console.log('Content as retrieved from the element', element)
+// Each section should be the size of the screen
+const ImageMosaicContainer = styled.section`
+  width: 100vw;
+  ${'' /* min-height: 100vh; */}
+  background-color: ${({ color, evenElement }) =>
+    evenElement ? 'white' : color};
 
-  return <MockSection>Single Image Section</MockSection>
+  display: grid;
+  grid-template-rows: 1fr 400px;
+  grid-row-gap: 10px;
+
+  .image-row-container {
+    padding: 20px;
+  }
+  .image-row {
+    display: grid;
+    padding: 10px;
+    background-color: white;
+    
+  }
+
+  .image {
+    ${'' /* min-width: 300px;
+    min-height: 300px; */}
+    max-height: 80vh;
+  }
+
+  .copy-container {
+    padding-left: 130px;
+
+
+    width: 85vw;
+    max-width: 800px; 
+
+    h3 {
+      color: ${({ color, altColor, evenElement }) =>
+        evenElement ? color : altColor};
+      font-weight: 900;
+      font-size: 1.2em;
+      margin-bottom: 0.4em;
+      text-transform: uppercase;
+    }
+  }
+`
+
+function CaseStudySectionMultipleImage(props) {
+  console.log('Case study single image props', props)
+  const { element, color, evenElement } = props
+
+  const { title, description, image, imagePosition } = element
+
+  const altColor =
+    color && compose(lightOrDark, getValueFromRGB, convertCSSToRGB)({ color })
+
+  return (
+    <ImageMosaicContainer
+      color={color}
+      altColor={altColor}
+      evenElement={evenElement}
+    >
+      <div className="image-row-container">
+        <div className="image-row">
+            <div className="image">
+              {image && <Img fluid={image.fluid} />}
+            </div>
+        </div>
+      </div>
+      {(title || description) && (
+        <div className="copy-container">
+          {title && <h3>{title}</h3>}
+          {description && <p>{description?.text}</p>}
+        </div>
+      )}
+    </ImageMosaicContainer>
+  )
 }
 
-CaseStudySectionSingleImage.propTypes = {}
+CaseStudySectionMultipleImage.propTypes = {}
 
-export default CaseStudySectionSingleImage
+export default CaseStudySectionMultipleImage
